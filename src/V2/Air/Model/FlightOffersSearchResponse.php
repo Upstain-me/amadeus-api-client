5426f61b014034aae8834697a0bf0e96d18f0caa
Lorant BENE <lbene@pitechnologies.ro>
2021-10-02 20:57:45 +0300

    Added some ideas.

--- src/V2/Air/Model/FlightOffersSearchResponse.php
+++ src/V2/Air/Model/FlightOffersSearchResponse.php
@@ -11,7 +11,14 @@ class FlightOffersSearchResponse
      */
     public array $data = [];
 
-
+    /**
+     * TODO add raw response and add format method
+     * each flight offer should contain the raw offer as well, so that we can use it on the price endpoint
+     * it will be nice for logging purposes as well
+     * TODO it's possible that this will have to be a service, because it will have to log
+     * TODO release schedule of library could involve just all the APIs with raw data format and then formatters afterwards
+     * if we have formatters, then those need to be services, and maybe added to the Amadeus entry point object
+     */
     public function __construct(array $data = [])
     {
         if (\count($data) > 0) {
